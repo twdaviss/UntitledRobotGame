7a46866c1654d1e66e210c2e4def0e8f1bda525d
Taylor Daviss <89953361+twdaviss@users.noreply.github.com>
2024-07-04 21:11:45 -0700

    Input Updates
    Controller Implementation + more

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float defaultMoveSpeed;
+    [SerializeField] private PlayerControls playerControls;
+    private Rigidbody2D playerRigidbody;
+    private Vector2 moveDirection;
+    private SpriteRenderer playerSprite;
+    private Animator playerAnimator;
+
+    private bool isSprinting = false;
+    private float moveSpeed;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    void Update()
+    {
+        InputHandler();
+        playerAnimator.SetFloat("Horizontal", moveDirection.x);
+        playerAnimator.SetFloat("Vertical", moveDirection.y);
+        playerAnimator.SetFloat("Speed", moveDirection.SqrMagnitude());
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);  
+    }
+
+    private void InputHandler()
+    {
+        playerControls.Gameplay.Move.performed += ctx => moveDirection = ctx.ReadValue<Vector2>();
+        playerControls.Gameplay.Move.canceled += ctx => moveDirection = Vector2.zero;
+
+        if (moveDirection.x < 0) { playerSprite.flipX = true; }
+        else { playerSprite.flipX = false; }
+
+        playerControls.Gameplay.Sprint.performed += ctx => {
+            isSprinting = !isSprinting;
+        };
+        if(moveDirection.magnitude == 0) { isSprinting = false; }
+        if (isSprinting) { moveSpeed = 2 * defaultMoveSpeed; }
+        else { moveSpeed = defaultMoveSpeed; }
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Gameplay.Disable();
+    }
+}
